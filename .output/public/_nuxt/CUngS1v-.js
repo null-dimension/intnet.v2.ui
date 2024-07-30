f6c1a7b4d1065457262823386ce73ce65b198f6c
null-dimension <4hrishi4@gmail.com>
2024-07-30 22:41:21 +0530

    temporarily commit build for deployment

---
+++ .output/public/_nuxt/CUngS1v-.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as n}from"./D7zSCYFi.js";const t={};function o(r,s){return n(),c("div",null," hello interests ")}const a=e(t,[["render",o]]);export{a as default};
---
+++ .output/public/_nuxt/D2fuUXey.js
@@ -0,0 +1,1 @@
+import{_ as e,c,o as r}from"./D7zSCYFi.js";const t={};function n(o,a){return r(),c("div",null," create ")}const _=e(t,[["render",n]]);export{_ as default};
