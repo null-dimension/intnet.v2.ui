3ee82650ec79433aa88c7ed4147f24a5299ad4a2
null-dimension <4hrishi4@gmail.com>
2024-08-01 10:42:02 +0530

    add login,register links

--- components/Header.vue
+++ components/Header.vue
@@ -12,6 +12,12 @@
             <NuxtLink to="/about" class="text-lg align-middle">About</NuxtLink>
           </li>
           <li>
+            <NuxtLink to="/login" class="text-lg align-middle">Login</NuxtLink>
+          </li>
+          <li>
+            <NuxtLink to="/register" class="text-lg align-middle">Register</NuxtLink>
+          </li>
+          <li>
             <ClientOnly>
               <UButton :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'" color="gray"
                 variant="ghost" aria-label="Theme" @click="isDark = !isDark" class="text-lg align-middle" />
